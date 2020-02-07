0f8bcb30be4f740e1305f297c15c432af94f4166
Lauren Doss <doss.laurene@gmail.com>
2020-02-06 18:25:22 -0800

    re-defined confirm and prompts

--- script.js
+++ script.js
@@ -16,50 +16,65 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = generatedPassword;
-  generatePassword();
-
 }
 
 function generatePassword () {
 
   var characterLength = prompt("Select a value between 8 and 128");
-  var upperCase = confirm("Would you like to use upper case letters?");
-  var lowerCase = confirm("Would you like to use lower case letters?");
-  var specialCharacters =  confirm("Would you like to use special characters?");
-  var selectNumbers = confirm("Would you like to use numbers?");
+  var upperCaseChoice = confirm("Would you like to use upper case letters?");
+  var lowerCaseChoice = confirm("Would you like to use lower case letters?");
+  var specialCharactersChoice =  confirm("Would you like to use special characters?");
+  var selectNumbersChoice = confirm("Would you like to use numbers?");
 
   // Check to make sure that the value is in the expected range
     //if (characterLength < 8 || characterLength > 128){}
  
-    if (upperCase === true) {
-      passwordCharacters.concat(upperCase);
+    if (upperCaseChoice === true) {
+      console.log(upperCase)
+      passwordCharacters = passwordCharacters.concat(upperCase)
+       console.log(passwordCharacters)
 
     }
-    else if (lowerCase === true) {
+    else if (lowerCaseChoice === true) {
       passwordCharacters.concat(lowerCase);
 
     }
-    else if (specialCharacters === true) {
+    else if (specialCharactersChoice === true) {
       passwordCharacters.concat(specialCharacters);
 
     }
-    else if (selectNumbers === true) {
+    else if (selectNumbersChoice === true) {
       passwordCharacters.concat(selectNumbers);
 
     }
 
-  var passwordLength = passwordCharacters;
+  //var passwordLength = passwordCharacters.length;
   var result = ""
   for ( var i = 0; i < characterLength; i++ ) {
-     result += passwordCharacters[(Math.floor(Math.random() * charactersLength))];
+    console.log(passwordCharacters)
+     result += passwordCharacters[(Math.floor(Math.random() * characterLength))]; console.log(result)
+
+  if (i = characterLength - 1) {
+    clearPassword (result)
+  }
    
    }
+   function clearPassword(result) {
+    document.getElementById("password").textContent=result;
+   }
+   
 
-   document.getElementById("password").textContent=result;
 
-    
+
   
-
+//on line 58 i need to add the password i generated on line 54 into the password text box in the Html 
+//it is much cleaner to use prompts instead of text boxes
+//input box for size of password
+//need to make sure i will be getting every type of character that i put into the password characters array. i
+//in "if" block , when I check to see if i they want something, add that to another array to be sure they 
+//are getting every type of character. Add array to result before I start the "for" loop to ensure they have
+//every type of character they ask for
+//add a check to make sure they have selected at least one type of character. If they select no for everything, no password will be generated.
 
   }
 
